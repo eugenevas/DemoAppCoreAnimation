fe7246fe93a6f8f9ad6b60bbf34dfb01bef58bbd
Evgeniy Vasilyev <eugene.vas25@gmail.com>
2020-03-09 15:13:07 +0300

    added ColorConstants

---
+++ DemoAppCoreAnimation/ColorConstants.swift
@@ -0,0 +1,63 @@
+//
+//  ColorConstants.swift
+//  DemoAppCoreAnimation
+//
+//  Created by Евгений Васильев on 09.03.2020.
+//  Copyright © 2020 Eugene Vasilyev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Colors {
+
+    static let red          = UIColor(
+                                      red: 255 / 255,
+                                      green: 115 / 255,
+                                      blue: 115 / 255,
+                                      alpha: 1)
+    
+    static let orange       = UIColor(
+                                      red: 255 / 255,
+                                      green: 175 / 255,
+                                      blue: 72 / 255,
+                                      alpha: 1)
+    
+    static let brightOrange = UIColor(
+                                      red: 255 / 255,
+                                      green: 69 / 255,
+                                      blue: 0 / 255,
+                                      alpha: 1)
+    
+    static let green        = UIColor(
+                                      red: 50 / 255,
+                                      green: 250 / 255,
+                                      blue: 50 / 255,
+                                      alpha: 1)
+    
+    static let darkGreen    = UIColor(
+                                      red: 0 / 255,
+                                      green: 120 / 255,
+                                      blue: 0 / 255,
+                                      alpha: 1)
+    
+    static let blue         = UIColor(
+                                      red: 30 / 255,
+                                      green: 144 / 255,
+                                      blue: 255 / 255,
+                                      alpha: 1)
+    
+    static let darkBlue     = UIColor(
+                                      red: 6 / 255,
+                                      green: 11 / 255,
+                                      blue: 155 / 255,
+                                      alpha: 1)
+    
+    static let purple       = UIColor(
+                                      red: 148 / 255,
+                                      green: 0 / 255,
+                                      blue: 211 / 255,
+                                      alpha: 1)
+    
+}
