bc224ddbc029d07d61efc45709451dc7c4727f42
Evgeniy Vasilyev <eugene.vas25@gmail.com>
2020-03-09 14:41:24 +0300

    Initial Commit

---
+++ DemoAppCoreAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAppCoreAnimation
+//
+//  Created by Евгений Васильев on 09.03.2020.
+//  Copyright © 2020 Eugene Vasilyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
